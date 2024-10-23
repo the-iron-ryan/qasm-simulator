9d2a8109ef563b6d49099ac2023b93d917ae797e
Ryan Dougherty <ryandougherty25@gmail.com>
2024-10-22 18:40:18 -0700

    Further progress on Ket

--- src/quantum/ket.rs
+++ src/quantum/ket.rs
@@ -2,5 +2,25 @@ use num::complex::Complex;
 
 #[derive(Debug, Hash, Eq, PartialEq)]
 pub struct Ket {
-    amp: Complex<i64>,
+    amplitude: Complex<i64>,
+    num_qubits: usize,
+    bits: Box<[bool]>,
+}
+
+#[macro_export]
+macro_rules! ket_arr {
+    ($x:expr ) => {
+        vec![false; $x].into_boxed_slice()
+    };
+}
+
+impl Ket {
+    fn new<T: Into<usize>>(num_qubits: T) -> Ket {
+        let num_qubits_size = num_qubits.into();
+        Ket {
+            amplitude: Complex::new(0, 0),
+            num_qubits: num_qubits_size,
+            bits: ket_arr!(num_qubits_size),
+        }
+    }
 }
